b0e80a9fe01905c14fab0655661925925405cc28
DrissaCAS <Drissa_cas@hotmail.com>
2026-01-29 14:30:11 -0300

    style: include page-level SCSS files

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Home = () => {
   return (
     <main>
-        <h1>Home</h1>
+        <h1>Projetos Sociais que transformam</h1>
+        <p>Conectamos sua empresa com projetos sociais impactantes. Juntos, criamos mudanças reais na comunidade.</p>
+        <button>
+          Cadastrar Empresa
+          
+        </button>
     </main>
   )
 }
