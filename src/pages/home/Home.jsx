b2599f71bec90143c5408ad078fe01fc188eb9b9
DrissaCAS <Drissa_cas@hotmail.com>
2026-02-04 19:40:21 -0300

    feat: update home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,34 +1,32 @@
 import S from "./home.module.scss"
-import imgHome from "../../assets/imagens/img_1.png"
+import imgHome from "../../assets/imagens/banner.png"
 
 const Home = () => {
   return (
     <main className={S.homeMain}>
-      <section className={S.homeTitle}>
+      <section className={S.homeInfo}>
         <h1>Projetos Sociais que transformam</h1>
         <p>Conectamos sua empresa com projetos sociais impactantes. Juntos, criamos mudanças reais na comunidade.</p>
-        <button>Cadastrar Empresa</button>
+        <button>Cadastrar Empresa ➔</button>
 
         <section className={S.cardInfo}>
           <article className={S.info}>
-            <h3>500+</h3>
-            <p>Empresas Voluntárias</p>
+            <h2>500+</h2>
+            <h4>Empresas Voluntárias</h4>
           </article>
           <article className={S.info}>
-            <h3>1.2K+</h3>
-            <p>Projetos Realizados</p>
+            <h2>1.2K+</h2>
+            <h4>Projetos Realizados</h4>
           </article>
           <article className={S.info}>
-            <h3>50K+</h3>
-            <p>Vidas Impactadas</p>
+            <h2>50K+</h2>
+            <h4>Vidas Impactadas</h4>
           </article>
         </section>
-        
       </section>
 
-      <section className={S.homeImg}>
-        <img src={imgHome} alt="Um dos símbolos do projeto social" />
-      </section>
+      <img src={imgHome} alt="Mãos segurando um globo terrestre dentro de uma corrente de papaeis coloridos em formato de bonecos representando projetos sociais e ambientais" />
+      
     </main>
   )
 }
